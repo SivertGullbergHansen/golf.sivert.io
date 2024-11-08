b4e6d17469b2c824b3d3d51192fb842be9ce0ba7
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2024-11-09 00:52:31 +0100

    Made baseline for index

--- assets/js/menuOpen.js
+++
@@ -1,7 +0,0 @@
-// Toggle the menu open and close when on mobile
-export default function menuOpen() {
-    const burgerButton = document.querySelector('.gh-burger');
-    burgerButton.addEventListener('click', function () {
-        document.body.classList.toggle('is-menu-open');
-    });
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.hbs", "./**/*.hbs"],
+  content: ["./*.hbs", "./partials/*.hbs"],
   theme: {
       extend: {
         screens: {
